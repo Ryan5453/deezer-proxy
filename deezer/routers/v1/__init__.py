687200ffac01a0c19fc7f6a1df1251f6d3f1e3ae
Ryan <git@ryan.science>
2024-09-20 01:36:51 -0400

    idek

--- deezer/routers/v1/__init__.py
+++ deezer/routers/v1/__init__.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Security
 
 from deezer.core.auth import get_api_key
 
-router = APIRouter(tags=["V1 API"])
-# router = APIRouter(dependencies=[Security(get_api_key)])
+router = APIRouter(tags=["V1 API"], dependencies=[Security(get_api_key)])
 
 from deezer.routers.v1.endpoints import *
